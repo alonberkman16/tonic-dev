04c54009690a0e8a816715fc23b66f82bfde78ee
Alon Berkman <alonberkman2004@gmail.com>
2025-12-12 10:25:42 +0200

    Added function documentation

--- src/utils.py
+++ src/utils.py
@@ -5,11 +5,26 @@ import time
 
 
 def get_jira_api_auth(email: str, api_token: str) -> HTTPBasicAuth:
+    """Uses HTTPBasicAuth to create an authentication object"""
     return HTTPBasicAuth(email, api_token)
 
 
 def run_request_with_error_handling(method: Literal['POST', 'GET'], url: str, headers: dict, params: dict,
                                     auth: HTTPBasicAuth, num_tries_if_error: int = 25, timeout_secs: int = 15) -> dict:
+    """
+    Runs a request with an error handling loop.
+    Handles both GET and POST requests.
+
+    :param method: The request method, can be one of: GET or POST
+    :param url: The url of the request
+    :param headers: The request headers
+    :param params: The request body/ parameters
+    :param auth: The authentication object
+    :param num_tries_if_error: The number of tries the function will use for consecutive fails
+    :param timeout_secs: The timeout for the request, if the number of seconds here reached, the code will stop the request
+
+    :return: The data received back from the api as JSON
+    """
     request_dict = {
         "method": method,
         "url": url,
